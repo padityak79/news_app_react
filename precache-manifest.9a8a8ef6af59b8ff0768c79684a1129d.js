697717fb2e959972f6373c2824517e05011f73f9
P Aditya Kumar <padityak79@gmail.com>
2020-06-19 18:47:58 +0530

    Updates

---
+++ precache-manifest.9a8a8ef6af59b8ff0768c79684a1129d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3533c96f7d76956b9bcb9dada8923480",
+    "url": "/news_app_react/index.html"
+  },
+  {
+    "revision": "aaecfb3f4afee391e47b",
+    "url": "/news_app_react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "60faee446eeb3b670b3a",
+    "url": "/news_app_react/static/js/2.8365c463.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/news_app_react/static/js/2.8365c463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaecfb3f4afee391e47b",
+    "url": "/news_app_react/static/js/main.d8b3ee67.chunk.js"
+  },
+  {
+    "revision": "a9e8430a824d35d61a02",
+    "url": "/news_app_react/static/js/runtime-main.bd9dede9.js"
+  }
+]);
